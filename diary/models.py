744a72fe8673644db8fed367df351473cdd6305e
jenny <88226911+kmjenny@users.noreply.github.com>
2023-05-31 17:13:30 +0900

    model 수정

--- diary/models.py
+++ diary/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from main.models import *
 
 class Diary(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     date = models.DateField()
     mood = models.IntegerField()
     content = models.TextField()
